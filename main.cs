cc52c80abc5e36c46e98606b8493a70837a2e1b3
patfr <56884994+patfr@users.noreply.github.com>
2020-08-21 08:18:50 +0200

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace system
+{
+    class MainClass 
+    {
+        public static void Main (string[] args) 
+        {
+            Console.Clear();
+            new Cpu().StartProgram();
+        }
+    }
+}
